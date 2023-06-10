589de096fc2e47816a4c490bd0bbd84083146230
nyx <sakusennx@gmail.com>
2023-06-10 12:46:39 +0900

    two-way connection.

--- src/client.ts
+++ src/client.ts
@@ -1,6 +1,25 @@
 import {io, Socket} from 'socket.io-client';
+import {Message} from "./model/message";
 
+// Initialize socket connection.
 const port: number = 5000;
 const socket: Socket = io(`http://localhost:${port}`);
 
-socket.on('connect', () => console.log('connect'));
+// Establishing socket connection.
+socket.on('connect', () : void => {
+    console.log('connect');
+});
+
+// Receive data from server via xxx event.
+socket.on('xxx', (data: Message): void => {
+    console.log(`type: ${typeof data}   data: ${data.message}`);
+});
+
+// Send message to server via yyy event
+let counter: number = 0;
+setInterval((): void => {
+    const data: Message = {
+        message: `client message ${counter++}`
+    }
+    socket.emit('yyy', data);
+}, 1000);
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,33 @@
 import http from 'http';
 import socketio from 'socket.io';
+import {Message} from "./model/message";
 
 const server: http.Server = http.createServer();
 const io: socketio.Server = new socketio.Server(server);
 
-io.on('connection', (socket: socketio.Socket) => console.log('connect'));
+const portNumber: number = 5000;
 
-const port: number = 5000;
-server.listen(port, () => console.log(`app listening on port ${port}`));
+// Start Http server.
+server.listen(portNumber, () : void => {
+    console.log(`app listening on port ${portNumber}`);
+});
+
+// Establish socket connection.
+io.on('connection', (socket: socketio.Socket) => {
+    console.log('connect');
+
+    let counter: number = 0;
+
+    // Receive data from client via yyy event.
+    socket.on('yyy', (data: Message): void => {
+        console.log(`type: ${typeof data}   data: ${data.message}`);
+    });
+
+    // Send message to client via xxx event.
+    setInterval((): void => {
+        const data: Message = {
+            message: `client message ${counter++}`
+        }
+        socket.emit('xxx', data);
+    }, 1000);
+});
